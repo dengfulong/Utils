982a765c5542106a9279dd28d26a50f4ae5bd2d6
dengfulong <852571503@qq.com>
2019-05-10 17:24:15 +0800

    666

--- src/com/zlb/axis/test/Produce.java
+++ src/com/zlb/axis/test/Produce.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 
 public class Produce implements Runnable {
 	
-	String url = "http://127.0.0.1:8080/axis2/services/RealTradeskldfjaslfjdsadfjksaljfdklafjlkasjflkasjdflaksjfdlkasjflkasjfdlkasdj";
+	String url = "http://127.0.0.1:8080/axis2/services/RealTrade";
 	String nm = "http://webservices.real.trade.zlb.com";
 	String method = "tradeInfo";
 	String xxx = "{123,456}";
